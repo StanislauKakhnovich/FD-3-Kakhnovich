a897b11714fafa34643b3b32fe0eb33180172e4e
Stanislau Kakhnovich <stas_2002@tut.by>
2022-04-19 17:52:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Homework for the React course. IT Academy.
---
+++ ishop/Ishop.js
@@ -0,0 +1,37 @@
+﻿var Ishop = React.createClass({
+
+  displayName: 'Ishop',
+
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    prosucts: React.PropTypes.array.isRequired,
+  
+    prosucts:React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        nameProduct: React.PropTypes.string.isRequired,
+        code: React.PropTypes.number.isRequired,
+        price: React.PropTypes.number.isRequired,
+        imgURL: React.PropTypes.string.isRequired,
+        quantity: React.PropTypes.number.isRequired,
+      })
+    )
+
+  },
+
+  render: function() {
+
+    var productsCode=this.props.products.map( elem =>
+        React.DOM.div({key:elem.code,className:'Product'},
+          React.DOM.span({className:'Description-Product'},elem.quantity),
+          React.DOM.img({className:'Description-Product', src:elem.imgURL}),
+          React.DOM.span({className:'Description-Product'},elem.price),
+          React.DOM.span({className:'Name'},elem.nameProduct),
+        )
+      );
+    return React.DOM.div( {className:'Ishop'}, 
+      React.DOM.div( {className:'Question'}, this.props.name ),
+      React.DOM.div( {className:'Products'}, productsCode ),
+    );
+  },
+
+});
