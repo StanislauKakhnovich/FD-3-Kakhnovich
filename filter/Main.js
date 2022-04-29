29dc6601c5570e6f0c495493d2ff790f8ee5b559
Stanislau Kakhnovich <stas_2002@tut.by>
2022-04-29 21:03:53 +0300

    object assign

--- filter/Main.js
+++ filter/Main.js
@@ -12,37 +12,39 @@
 
   getInitialState: function() {
     return { 
-      words: this.props.startWords,
-      arr: this.props.startWords,
+      arrEtalon: Object.values(Object.assign({},this.props.startWords)),
+      wordRender: this.props.startWords,
     };
   },
 
   sortWords: function(EO) {
     
     if (EO.target.checked) {
-      this.setState( {words: this.state.words.sort()} ); 
+
+      this.setState( {wordRender: this.state.wordRender.sort()} );
+      console.log(this.state.arrEtalon);
       
     }
     if (!EO.target.checked) {
-      this.setState( {words: this.state.arr} ); 
-      console.log(this.state.words);
+      this.setState( {wordRender: this.state.arrEtalon} ); 
     }
     
   },
 
   resetWords: function() {
-    this.setState( {words: this.state.arr} );
-    console.log(this.state.arr);
+    this.setState( {wordRender: this.state.arrEtalon} ); 
+    
   },
 
   changeList: function(EO) {
-    this.setState( {words: this.state.words.filter(elem => elem.includes(EO.target.value))} );
+    
+    this.setState( {wordRender: this.state.wordRender.filter(elem => elem.includes(EO.target.value))} );
     
   },
 
   render: function() {
 
-    var wordsCode=this.state.words.map( (elem, index) =>
+   var wordCode = this.state.wordRender.map( (elem, index) =>
       React.createElement(List, {key: index,
         word: elem,
         valueNumber: index,
@@ -53,7 +55,7 @@
       React.DOM.input({type:'checkbox', className:'Checkbox',onClick:this.sortWords}),
       React.DOM.input({type:'text', name:'inputtext', className:'Text',onChange:this.changeList}),
       React.DOM.input( {type:'button',value:'Сброс',className:'Reset',onClick:this.resetWords}),
-      React.DOM.select( {name:'selecttext',className:'Words', size:this.state.words.length}, wordsCode ),
+      React.DOM.select( {name:'selecttext',className:'Words', size:this.state.wordRender.length}, wordCode ),
     );
   },
 
