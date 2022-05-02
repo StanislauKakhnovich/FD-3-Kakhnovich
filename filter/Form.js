26cb9be035f34351627fca94199717c3105aff0a
Stanislau Kakhnovich <stas_2002@tut.by>
2022-05-02 16:15:02 +0300

    the right decision

---
+++ filter/Form.js
@@ -0,0 +1,87 @@
+var Form = React.createClass({
+
+    displayName: 'Form',
+  
+    propTypes: {
+    //   sortFlag:React.PropTypes.bool.isRequired,
+    //   filterStr:React.PropTypes.string.isRequired,
+    //   lines: React.PropTypes.array.isRequired,
+      linesOriginal:React.PropTypes.array.isRequired,
+      linesOriginal:React.PropTypes.arrayOf(
+        React.PropTypes.string.isRequired,
+      ),
+  
+    },
+  
+  
+    getInitialState: function() {
+      return { 
+        filterStr: "",
+        sortFlag: false,
+        lines: this.props.lines,
+      };
+    },
+  
+    sortFlagChanged: function (eo) {
+      this.setState ({sortFlag: eo.target.checked}, this.processList);
+    },
+  
+    filterStrChanged: function (eo) {
+      this.setState ({filterStr: eo.target.value}, this.processList);
+      console.log(this.state.filterStr);
+    },
+  
+    processList: function() {
+      var arr = this.props.lines;
+      if (this.state.sortFlag) {
+        arr.sort();
+        if (this.state.filterStr) {
+          arr = arr.filter(elem => elem.includes(this.state.filterStr));
+        }
+        this.setState ({lines: arr}); 
+      } 
+      else if (this.state.filterStr) {
+        arr = arr.filter(elem => elem.includes(this.state.filterStr));
+          if (this.state.sortFlag) {
+           arr.sort();
+         }
+        this.setState ({lines: arr}); 
+      } else {
+        this.setState ({lines: this.props.linesOriginal}); 
+      }
+    },
+  
+    reset: function() {
+      this.setState({sortFlag: false, filterStr: ""}, this.processList)
+    },
+  
+  
+    render: function() {
+     var wordCode = this.state.lines.map( (elem, index) =>
+        React.createElement(List, {key: index,
+          word: elem,
+          valueNumber: index,
+          })
+        );
+  
+      return React.DOM.div( {className:'Form'}, 
+        React.DOM.input({type:'checkbox',
+          className:'Checkbox',
+          checked:this.state.sortFlag,
+          onChange:this.sortFlagChanged}),
+        React.DOM.input({type:'text', 
+          name:'inputtext', 
+          className:'Text',
+          value:this.state.filterStr,
+          onChange:this.filterStrChanged}),
+        React.DOM.input( {type:'button',
+          value:'Сброс',
+          className:'Reset',
+          onClick:this.reset}),
+        React.DOM.select( {name:'selecttext',
+          className:'Words', 
+          size:this.state.lines.length}, wordCode ),
+      );
+    },
+  
+  });
--- filter/Main.js
+++ filter/Main.js
@@ -3,59 +3,24 @@
   displayName: 'Main',
 
   propTypes: {
-    startWords:React.PropTypes.array.isRequired,
-    startWords:React.PropTypes.arrayOf(
+    linesOriginal:React.PropTypes.array.isRequired,
+    linesOriginal:React.PropTypes.arrayOf(
       React.PropTypes.string.isRequired,
     ),
 
   },
-
   getInitialState: function() {
     return { 
-      arrEtalon: Object.values(Object.assign({},this.props.startWords)),
-      wordRender: this.props.startWords,
+      lines: Object.values(Object.assign({},this.props.linesOriginal)),
     };
   },
-
-  sortWords: function(EO) {
-    
-    if (EO.target.checked) {
-
-      this.setState( {wordRender: this.state.wordRender.sort()} );
-      console.log(this.state.arrEtalon);
-      
-    }
-    if (!EO.target.checked) {
-      this.setState( {wordRender: this.state.arrEtalon} ); 
-    }
-    
-  },
-
-  resetWords: function() {
-    this.setState( {wordRender: this.state.arrEtalon} ); 
-    
-  },
-
-  changeList: function(EO) {
-    
-    this.setState( {wordRender: this.state.wordRender.filter(elem => elem.includes(EO.target.value))} );
-    
-  },
-
+  
   render: function() {
-
-   var wordCode = this.state.wordRender.map( (elem, index) =>
-      React.createElement(List, {key: index,
-        word: elem,
-        valueNumber: index,
-        })
-      );
-
+ 
     return React.DOM.div( {className:'Main'}, 
-      React.DOM.input({type:'checkbox', className:'Checkbox',onClick:this.sortWords}),
-      React.DOM.input({type:'text', name:'inputtext', className:'Text',onChange:this.changeList}),
-      React.DOM.input( {type:'button',value:'Сброс',className:'Reset',onClick:this.resetWords}),
-      React.DOM.select( {name:'selecttext',className:'Words', size:this.state.wordRender.length}, wordCode ),
+    React.createElement( Form, {linesOriginal:this.props.linesOriginal,
+      lines: this.state.lines,
+    } ),
     );
   },
 
