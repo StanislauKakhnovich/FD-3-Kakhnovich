249a19ffb3d4c503c661ea1e217fd8249e3b4af3
Stanislau Kakhnovich <stas_2002@tut.by>
2022-05-20 15:18:26 +0300

    without if

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -4,14 +4,9 @@ import PropTypes from 'prop-types';
 
 let withRainbowFrame= colors => Comp => props =>
   {
-    let control;
+    let control=<Comp {...props} />;
      colors.forEach((elem, i)=>{
-      if(i==0){
-        control = <div key={i}  style={{border:"solid 3px "+elem,padding:"5px"}}><Comp {...props} /></div>;
-      } 
-      else {
         control = <div key={i}  style={{border:"solid 3px "+elem,padding:"5px"}}>{control}</div>;
-      } 
     })
 
     return control;
